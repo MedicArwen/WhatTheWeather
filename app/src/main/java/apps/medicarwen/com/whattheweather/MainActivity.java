9fe7627368dd4cf294abb0cf2c2d9a5523db16dd
MedicArwen <thierry-bru@laposte.net>
2018-12-06 15:22:21 +0100

    gestionnaire d'événement ajouté pour gérer le click

--- app/src/main/java/apps/medicarwen/com/whattheweather/MainActivity.java
+++ app/src/main/java/apps/medicarwen/com/whattheweather/MainActivity.java
@@ -13,35 +13,41 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.widget.Toast.*;
+
 public class MainActivity extends AppCompatActivity {
     private TextView mTextViewCityName;
     private TextView mTextConnection;
     private LinearLayout mLayoutOrange;
     private Button mBoutonFavoris;
+    private Button mBouton1;
+    private Button mBouton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.mTextViewCityName=(TextView)findViewById(R.id.text_view_city_name);
+        this.mTextViewCityName =findViewById(R.id.text_view_city_name);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                makeText(v.getContext(), "bouton Favoris", LENGTH_SHORT).show();
+            }
+        };
+        this.mBoutonFavoris =findViewById(R.id.buttonBoutonFavoris);
+        this.mBouton1 =findViewById(R.id.buttonBouton1);
+        this.mBouton2 =findViewById(R.id.buttonBouton2);
+        this.mBouton1.setOnClickListener(onClickListener);
+        this.mBouton2.setOnClickListener(onClickListener);
+        this.mBoutonFavoris.setOnClickListener(onClickListener);
         mTextViewCityName.setText(R.string.city_name);
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo =connManager.getActiveNetworkInfo();
-        if (networkInfo!=null&&networkInfo.isConnected())
-        {
-            //connecté au net
-            Log.d("TAG","connecté");
-
-
-        }
-        else
-        {
-            //not connecté
-            Log.d("tag","non connecté");
-
-            this.mTextConnection=(TextView)findViewById(R.id.text_view_networkstatus);
-            this.mBoutonFavoris=(Button)findViewById(R.id.buttonBoutonFavoris);
-            this.mLayoutOrange=(LinearLayout)findViewById(R.id.layoutOrange);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            this.mTextConnection = (TextView) findViewById(R.id.text_view_networkstatus);
+            this.mLayoutOrange = (LinearLayout) findViewById(R.id.layoutOrange);
 
             this.mTextConnection.setVisibility(View.VISIBLE);
             this.mTextConnection.setText(R.string.text_about_not_connected);
@@ -50,7 +56,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void actionClickBouton1(View view) {
-        //Toast.makeText(this, "YOUPLABOOM", Toast.LENGTH_SHORT).show();
-    }
 }
