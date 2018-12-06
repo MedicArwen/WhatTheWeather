abf6c0a908fc7e85203c9d5c66505dc53d96d4a1
MedicArwen <thierry-bru@laposte.net>
2018-12-06 20:05:21 +0100

    gestionnaire d'événement ajouté pour gérer le click

--- app/src/main/java/apps/medicarwen/com/whattheweather/MainActivity.java
+++ app/src/main/java/apps/medicarwen/com/whattheweather/MainActivity.java
@@ -1,9 +1,11 @@
 package apps.medicarwen.com.whattheweather;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.IdRes;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,28 +21,25 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextViewCityName;
     private TextView mTextConnection;
     private LinearLayout mLayoutOrange;
-    private Button mBoutonFavoris;
-    private Button mBouton1;
-    private Button mBouton2;
+    private FloatingActionButton mBoutonFavoris;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.mTextViewCityName =findViewById(R.id.text_view_city_name);
+        this.mBoutonFavoris = findViewById(R.id.boutonActionFlottant);
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                makeText(v.getContext(), "bouton Favoris", LENGTH_SHORT).show();
+                Intent intent = new Intent(v.getContext(),FavoriteActivity.class);
+                startActivity(intent);
+                //Intent intent = new Intent(this, FavoriteActivity.class); startActivity(intent);
             }
         };
-        this.mBoutonFavoris =findViewById(R.id.buttonBoutonFavoris);
-        this.mBouton1 =findViewById(R.id.buttonBouton1);
-        this.mBouton2 =findViewById(R.id.buttonBouton2);
-        this.mBouton1.setOnClickListener(onClickListener);
-        this.mBouton2.setOnClickListener(onClickListener);
+
         this.mBoutonFavoris.setOnClickListener(onClickListener);
         mTextViewCityName.setText(R.string.city_name);
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -48,10 +47,9 @@ public class MainActivity extends AppCompatActivity {
         if (networkInfo == null || !networkInfo.isConnected()) {
             this.mTextConnection = (TextView) findViewById(R.id.text_view_networkstatus);
             this.mLayoutOrange = (LinearLayout) findViewById(R.id.layoutOrange);
-
             this.mTextConnection.setVisibility(View.VISIBLE);
             this.mTextConnection.setText(R.string.text_about_not_connected);
-            this.mBoutonFavoris.setVisibility(View.INVISIBLE);
+            this.mBoutonFavoris.hide();
             this.mLayoutOrange.setVisibility(View.INVISIBLE);
         }
     }
