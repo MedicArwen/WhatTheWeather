3530a54c947f80cea4d6c8e6a303c3a9715a0bf0
MedicArwen <thierry-bru@laposte.net>
2018-12-14 10:23:18 +0100

    version unstable mise en place persistance sql

---
+++ app/src/main/java/apps/medicarwen/com/whattheweather/DataBaseHelper/DataBaseHelper.java
@@ -0,0 +1,104 @@
+package apps.medicarwen.com.whattheweather.DataBaseHelper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+import apps.medicarwen.com.whattheweather.models.AirInfo;
+import apps.medicarwen.com.whattheweather.models.City;
+import apps.medicarwen.com.whattheweather.models.Coord;
+import apps.medicarwen.com.whattheweather.models.Weather;
+
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    private final static String DATABASE_NAME="WeatherDatabase";
+    private final static int DATABASE_VERSION=0;
+
+
+    private static final String TABLE_CITY = "city";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_ID_CITY = "id_city";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_TEMP = "temp";
+    private static final String KEY_DESC = "desc";
+    private static final String KEY_RES_ICON = "res_icon";
+    private static final String KEY_LAT = "lat";
+    private static final String KEY_LNG = "lng";
+
+    private final static String CREATE_TABLE_CITY="CREATE TABLE "+TABLE_CITY+" ("
+            + KEY_ID + " INTEGER PRIMARY KEY," + KEY_ID_CITY + " INTEGER,"
+            + KEY_NAME + " TEXT,"
+            + KEY_TEMP + " TEXT,"
+            + KEY_DESC+" TEXT,"
+            + KEY_RES_ICON + " INTEGER,"
+            + KEY_LAT + " DECIMAL (3, 10),"
+            + KEY_LNG + " DECIMAL (3, 10))";
+
+    public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_CITY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_CITY);
+        onCreate(db);
+    }
+
+    public long insertCity(City pCity)
+    {
+        long retour;
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues insertValues = new ContentValues();
+        insertValues.put(KEY_ID_CITY,pCity.mCityId); //0
+        insertValues.put(KEY_NAME,pCity.mName); //1
+        insertValues.put(KEY_TEMP,pCity.mInfoAir.temp); //2
+        insertValues.put(KEY_DESC,pCity.mMeteo.description); //3
+        insertValues.put(KEY_RES_ICON,pCity.mMeteo.icon); //4
+        insertValues.put(KEY_LAT,pCity.mCoordonnees.lat);//5
+        insertValues.put(KEY_LNG,pCity.mCoordonnees.lon);//6
+        retour = db.insert(TABLE_CITY,null,insertValues);
+        db.close();
+        return retour;
+    }
+    public int deleteCity(int pIdCity)
+    {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        int nbrowsDeleted;
+        nbrowsDeleted= db.delete(TABLE_CITY,KEY_ID_CITY+"= ?",new String[]{Integer.toString(pIdCity)});
+        db.close();
+        return nbrowsDeleted;
+    }
+    public ArrayList<City> selectAllCities()
+    {
+        ArrayList<City> cityList= new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.query(TABLE_CITY,null,null,null,null,null,null);
+        if (cursor.moveToFirst())
+        {
+            do {
+                Weather meteo = new Weather(cursor.getString(3),cursor.getString(4));
+                Coord coord = new Coord(cursor.getDouble(6),cursor.getDouble(5));
+                AirInfo airInfo= new AirInfo(cursor.getDouble(2));
+                //   City newCity = new City(cursor.getString(1),cursor.getInt(1),airInfo,meteo,coord,cursor.getInt(0));
+                // cityList.add(newCity);
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return cityList;*/
+        return null;
+    }
+
+
+}
